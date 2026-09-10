0ecb6872abfb1b932ea2f8dea0dbe049feb2ec25
vlik35 <58407620+vlik35@users.noreply.github.com>
2026-09-10 18:44:57 -0500

    Update lavalink configuration in config.js

--- config.js
+++ config.js
@@ -26,11 +26,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
       {
-      name: "GlaceYT",
-      password: "glace",
-      host: "de-01.strixnodes.com",
-      port: 2010,
-      secure: false
+      name: "archost",
+      password: "https://discord.gg/archost",
+      host: "89.106.84.47",
+      port: "2555",
+      secure: "false" 
     }
   ]
 }

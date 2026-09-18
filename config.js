645ef6025c0ea53aa92031deaaa5bdf79976cc69
vlik35 <58407620+vlik35@users.noreply.github.com>
2026-09-18 12:59:48 -0500

    Update node configuration in config.js
    Replaced existing node configuration with new DEVOURY settings.

--- config.js
+++ config.js
@@ -26,11 +26,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
       {
-         name: "archost",
-         password: "https://discord.gg/archost",
-         host: "89.106.84.47",
-         port: "2555",
-         secure: "false"
+          name: "DEVOURY",
+          password: "md",
+          host: "node.fynexbot.in",
+          port: 2007,
+          secure: false
     }
   ]
 }
